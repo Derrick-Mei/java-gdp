7742add2c5fc7ad59bfb028227f68a3224f2d89b
Derrick Mei <derrick.mei.168@gmail.com>
2019-02-17 03:12:25 -0600

    derrick-mei
    initial commit: created template

---
+++ src/main/java/com/dkm/gdpartifact/AaGdpNameApplication.java
@@ -0,0 +1,15 @@
+package com.dkm.gdpartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaGdpNameApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AaGdpNameApplication.class, args);
+    }
+
+}
